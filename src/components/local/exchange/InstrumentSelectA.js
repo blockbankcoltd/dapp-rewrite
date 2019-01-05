758bd22fbc1085d0b66f914fb2963b54f9d506ac
Aman Mahalgavan <aman.mahalgavan1991@gmail.com>
2019-01-05 12:38:12 +0900

    moved token logo imports from instruementA component to the config.

--- src/components/local/exchange/InstrumentSelectA.js
+++ src/components/local/exchange/InstrumentSelectA.js
@@ -4,18 +4,6 @@ import { isMobile } from 'react-device-detect';
 import updown_1 from "../../../assets/images/updown_1.png";
 import updown_2 from "../../../assets/images/updown_2.png";
 import updown_3 from "../../../assets/images/updown_3.png";
-import ETH from "../../../assets/images/icon/eth.png";
-import LNC from "../../../assets/images/icon/lnc.png";
-import BAT from "../../../assets/images/icon/bat.png";
-import OMG from "../../../assets/images/icon/omg.png";
-import VIKKY from "../../../assets/images/icon/vikky.png";
-import BTCB from "../../../assets/images/icon/btcb.png";
-import NDI from "../../../assets/images/icon/vote1.png";
-import VAT from "../../../assets/images/icon/cymt.png";
-import BNB from "../../../assets/images/icon/bnb.png";
-import ICON from "../../../assets/images/icon/icon.png";
-import TUSD from "../../../assets/images/icon/tusd.png";
-import VIEN from "../../../assets/images/icon/wab.png";
 
 
 export default class InstrumentSelectA extends React.Component {
@@ -80,8 +68,6 @@ export default class InstrumentSelectA extends React.Component {
     }
 
     componentDidMount() {
-        console.log("Data from HOC ----> ", this.props.data);
-        // let tradeData = this.props.data && this.props.data.length > 0 ? this.props.data[0].market.trades : [];
         this.setState(function (state, props) {
             return {
                 initialData: props.data,
@@ -90,8 +76,6 @@ export default class InstrumentSelectA extends React.Component {
             }
         });
 
-        // this.props.handleTradeCurrencyChange(this.props.data[0].market.productId, tradeData[0].productId)
-        // this.props.handleTradeCurrencyChange(this.props.data[0].market.productId, tradeData[0].productId)
     }
 
     componentDidUpdate(prevProps) {
@@ -133,7 +117,6 @@ export default class InstrumentSelectA extends React.Component {
                                     key={idx}
                                     className={this.state.activeTab === idx ? "active" : ""}
                                     onClick={() => {
-                                        console.log("Active tab data ==> ", this.props.data[idx])
                                         this.props.changeTabData(obj.market.productId);
                                         this.setState({
                                             activeTabData: this.props.data[idx].market.trades,
@@ -192,45 +175,13 @@ export default class InstrumentSelectA extends React.Component {
                                 return (
                                 <tr
                                     key={i}
-                                    // onClick={() => this.props.handleTradeCurrencyChange(this.state.activeBaseToken, obj.productId)}
                                     onClick={() => this.props.handleTradeCurrencyChange(this.state.activeBaseToken, obj.productId)}
                                     className="instrument-*coinSymbol"
                                     style={{border: "none", fontWeight: "bold", display: "table-row"}}
                                 >
                                     <td className="coinSymbol">
-                                        {
-                                            obj.productName === "BAT" &&
-                                            <img src={BAT}/>
-                                        }
-                                        {
-                                            obj.productName === "OMG" &&
-                                            <img src={OMG}/>
-                                        }
-                                        {
-                                            obj.productName === "VIKKY" &&
-                                            <img src={VIKKY}/>
-                                        }
-                                        {
-                                            obj.productName === "BTCB" &&
-                                            <img src={BTCB}/>
-                                        }
-                                        {
-                                            obj.productName === "NDI" &&
-                                            <img src={NDI}/>
-                                        }
-                                        {
-                                            obj.productName === "VAT" &&
-                                            <img src={VAT}/>
-                                        }
-                                        {
-                                            obj.productName === "BNB" &&
-                                            <img src={BNB}/>
-                                        }
-                                        {
-                                            obj.productName === "ICON" &&
-                                            <img src={ICON}/>
-                                        }
-                                    {obj.productName}
+                                        <img src={obj.logo}/>
+                                        {obj.productName}
                                     </td>
                                     <td className="up dataNumber">{obj.bestBid}</td>
                                     <td className="up dataNumber">{obj.bestAsk}</td>
@@ -245,123 +196,6 @@ export default class InstrumentSelectA extends React.Component {
         )
     }
 }
-
-
-
-
-// import * as React from 'react'
-// import styled from 'styled-components';
-// export default class InstrumentSelectA extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {isMobile: false}
-//     }
-
-//     componentDidMount() {
-//         console.log("Data Coming from HOC ---> ", this.props)
-//         this.setState( (state, props) => {
-//             return {tabData: props.data[0].market.trades}
-//         }) 
-//         console.log("Data Coming from HOC ---> ", this.props.data[0].market.productName)
-//     }
-
-//     render() {
-//         let timer = null;
-//         const { INSTRUMENTS } = this.props.languageConfig;
-//         return (
-//             <Selector className="mobileWrapper">
-//             {this.state.isMobile ? 
-//                 (
-//                 <button className="setCoinMarket" onClick={this.onViewMarketList}>
-//                     <span className="marketSelector" />
-//                     <i className="xi-caret-down" />
-//                 </button>
-//                 )
-//                 : ''}
-//             <div id="mobileInst" className={this.state.isMobile ? 'coinsWrap mobile' : 'coinsWrap'}>
-//               <div className="coinsTab">
-//                 {this.props.data.map((obj, idx) => {
-//                             return (
-//                               <button
-//                                 key={obj.market.productName}
-//                                 className={this.state.activeTab === idx ? "active" : ""}
-//                                 onClick={() => {
-//                                             this.setState({
-//                                                 activeTabData: obj.market.trades,
-//                                                 activeBaseToken: obj.market.productId,
-//                                                 activeTab: idx
-//                                             })
-//                                         }}
-//                               >
-//                                 {obj.market.productName}
-//                               </button>
-//                             )
-//                         })}
-//               </div>
-//               <table className="coinTable">
-//                 <thead>
-//                   <tr>
-//                     <th
-//                       width="34%"
-//                     //   onClick={() => {
-//                     //             this.selectSort('char');
-//                     //         }}
-//                     >
-//                       {INSTRUMENTS.COIN_NAME}
-//                       {/* <img
-//                         src={this.selectImage('char')}
-//                       /> */}
-//                     </th>
-//                     <th
-//                       width="33%"
-//                     //   onClick={() => {
-//                     //             this.selectSort('bid');
-//                     //         }}
-//                     >
-//                       {INSTRUMENTS.BEST_BID}
-//                       {/* <img
-//                         src={this.selectImage('price')}
-//                       /> */}
-//                     </th>
-//                     <th
-//                       width="33%"
-//                     //   onClick={() => {
-//                     //             this.selectSort('ask');
-//                     //         }}
-//                     >
-//                       {INSTRUMENTS.BEST_ASK}
-//                       {/* <img
-//                         src={this.selectImage('percent')}
-//                       /> */}
-//                     </th>
-
-//                   </tr>
-
-//                 </thead>
-//                 <tbody>
-//                   {this.props.data[0].market.trades.map( obj => {
-//                             return (
-//                               <tr
-//                                 key={obj.productName}
-//                                 // onClick={() => this.props.handleTradeCurrencyChange(this.state.activeBaseToken, obj.productId)}
-//                                 // onClick={() => this.props.handleTradeCurrencyChange(this.state.activeBaseToken, obj.productId)}
-//                                 className="instrument-*coinSymbol"
-//                                 style={{border: "none", fontWeight: "bold", display: "table-row"}}
-//                               >
-//                                 <td className="coinSymbol">{obj.productName}</td>
-//                                 <td className="up dataNumber">{obj.price}</td>
-//                                 <td className="up dataNumber">{obj.change}</td>
-//                               </tr>
-//                             )
-//                         })}
-//                 </tbody>
-
-//               </table>
-//             </div>
-//           </Selector>
-//         )
-//     }
-// }
 
 const Selector = styled.div`
     background: #fff;
--- src/utilities/config.js
+++ src/utilities/config.js
@@ -1,6 +1,18 @@
 import * as contractJson2 from '../utilities/DEXHIGH2.json';
 import * as contractJson3 from '../utilities/DEXHIGH3.json';
 
+import ETH from "../assets/images/icon/eth.png";
+import LNC from "../assets/images/icon/lnc.png";
+import BAT from "../assets/images/icon/bat.png";
+import OMG from "../assets/images/icon/omg.png";
+import VIKKY from "../assets/images/icon/vikky.png";
+import BTCB from "../assets/images/icon/btcb.png";
+import NDI from "../assets/images/icon/vote1.png";
+import VAT from "../assets/images/icon/cymt.png";
+import BNB from "../assets/images/icon/bnb.png";
+import ICON from "../assets/images/icon/icon.png";
+import TUSD from "../assets/images/icon/tusd.png";
+import VIEN from "../assets/images/icon/wab.png";
 
 export const config = {
 
@@ -12,70 +24,70 @@ export const config = {
             decimal: "1000000000000000000",
             productId: 3,
             tokenAddress: "0x8f803178891e51e055a4542788678b134435b613",
-            logo: ""
+            logo: BAT
         },
         {
             productName: "OMG",
             decimal: "1000000000000000000",
             productId: 4,
             tokenAddress: "0x10e4437cd6ab32d11a4b0d983157fe1dffc673c4",
-            logo: ""
+            logo: OMG
         },
         {
             productName: "VIKKY",
             decimal: "1000000000000000000",
             productId: 5,
             tokenAddress: "0x873f4f3105377427c47c643221e7f250a5b4ddcf",
-            logo: ""
+            logo: VIKKY
         },
         {
             productName: "BTCB",
             decimal: "1000000000000000000",
             productId: 6,
             tokenAddress: "0xb15596d37ec8ca879dc9bfbc1da92331ca6497be",
-            logo: ""
+            logo: BTCB
         },
         {
             productName: "NDI",
             decimal: "1000000000000000000",
             productId: 7,
             tokenAddress: "0xc4c4c06ffad6f5e4099c754d1e58d6cec6d20b5f",
-            logo: ""
+            logo: NDI
         },
         {
             productName: "VAT",
             decimal: "1000000000000000000",
             productId: 8,
             tokenAddress: "0x3c6eb741bfec2f7916f6ea9b84f1494b02fe23d8",
-            logo: ""
+            logo: VAT
         },
         {
             productName: "BNB",
             decimal: "1000000000000000000",
             productId: 9,
             tokenAddress: "0xd2aff1f2debe1120b4674b0b63e822dfc421c446",
-            logo: ""
+            logo: BNB
         },
         {
             productName: "ICON",
             decimal: "1000000000000000000",
             productId: 10,
             tokenAddress: "0xcfc701dd1570e5bf7fff2e57176e7dae7c858132",
-            logo: ""
+            logo: ICON
         },
         {
             productName: "TUSD",
             decimal: "1000000000000000000",
             productId: 11,
             tokenAddress: "0xb7c6ef39ba505bfdb560f0cd32c84963e3d8bcbe",
-            logo: ""
+            logo: TUSD
         },
         {
             productName: "VIEN",
             decimal: "1000000000000000000",
             productId: 12,
             tokenAddress: "0x13f59e0ed9224f646a94f28ca8120fc011b890b8",
-            logo: ""
+            logo: VIEN
         },
         {
 
@@ -86,13 +98,15 @@ export const config = {
             productName: "ETH",
             productId: 1,
             tokenAddress: "0x0000000000000000000000000000000000000000",
-            decimal: "1000000000000000000"
+            decimal: "1000000000000000000",
+            logo: ETH
         },
         {
             productName: "LNC",
             productId: 2,
             tokenAddress: "0x81d0a10c4478d6d0aea604aa4aa5a3154efa5bbd",
-            decimal: "1000000000000000000"
+            decimal: "1000000000000000000",
+            logo: LNC
         }
     ],
     market: [
